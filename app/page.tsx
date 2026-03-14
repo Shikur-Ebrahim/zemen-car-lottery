25abcf30e8b777cb0dc476025c8b0f823f595d8a
Propertymanagementsystem1 <propertymanagementsystem1@gmail.com>
2026-03-14 12:26:07 +0300

    update project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Zemen Car Lottery
+      </h1>
+    </main>
+  );
+}
